dffd3c94066a987a809b3f472d4d96e8cf9993bf
Sacha924 <sacha.simon@edu.devinci.fr>
2022-12-25 13:13:47 +0100

    Allow user to set the threshold param

--- pages/main.js
+++ pages/main.js
@@ -1,32 +1,54 @@
-import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
 import initSqlJs from "sql.js";
 import fs from "fs";
 
 export default function Main(props) {
   // console.log(props.request[0]);
+  const [threshold, setThreshold] = useState(10000);
+
+  const inputRef_threshold = useRef();
   const data = {
     columns: props.request[0].columns,
     values: props.request[0].values,
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setThreshold(inputRef_threshold.current.value);
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          {data.columns.map((column) => (
-            <th key={column}>{column}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.values.map((row) => (
-          <tr key={row[0]}>
-            {row.map((cell) => (
-              <td key={cell}>{cell}</td>
+    <div className="container">
+      <h1 className="title">Price Anomaly</h1>
+      <table>
+        <thead>
+          <tr>
+            {data.columns.map((column) => (
+              <th key={column}>{column}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.values.map(
+            (row) =>
+              Math.abs(row[2]) < threshold && (
+                <tr key={row[0]}>
+                  {row.map((cell) => (
+                    <td key={cell}>{cell}</td>
+                  ))}
+                </tr>
+              )
+          )}
+        </tbody>
+      </table>
+      <form id="userAnomalySetting" onSubmit={handleSubmit}>
+        <label htmlFor="threshold">Set a positive value for the num_stddev (It will change how many standard deviations a data point from the mean is considered abnormal)</label>
+        <input type="number" name="threshold" min={"0"} ref={inputRef_threshold} />
+        <input type="submit" id="submit" value="Submit" />
+      </form>
+
+      <p style={{ position: "absolute", bottom: "10px" }}>Note : the values with id 1 and 2 are used for test only, they are faked</p>
+    </div>
   );
 }
 
